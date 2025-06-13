09e49f462ada10478b30d927e2a9909ffd682d86
Carol <carolsofiahoyos@gmail.com>
2025-06-13 06:31:07 -0500

    Ruta temporal para ejecutar migraciones en Render

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::view('/{any?}', 'welcome')->where('any', '.*');
+
+Route::get('/migrar', function () {
+    Artisan::call('migrate --force');
+    Artisan::call('db:seed --force');
+    return 'Migración y seed ejecutados';
+});
 
 {/*Route::get('/login', function () {
     return response()->json(['message' => 'No autenticado.'], 401);
